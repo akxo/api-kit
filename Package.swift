d905b1475c3297f6d2250d0ae0795821fd12011a
ak <atrix113@aol.com>
2023-05-05 23:03:56 -0400

    refactors project structure

--- Package.swift
+++ Package.swift
@@ -23,9 +23,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "APIKit",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "APIKitTests",
-            dependencies: ["APIKit"]),
+            dependencies: ["APIKit"],
+            path: "Tests"),
     ]
 )
